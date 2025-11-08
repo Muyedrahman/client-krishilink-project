fcda45651a820189498cd58d808a9583b2fc994b
Muyedur rahman <muyedrahman732@gmail.com>
2025-11-09 00:55:51 +0600

    add nav-bar

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1>B12-A10</h1>
+      <h1>KrishiLink- Farmer’s Growth &amp; Connection Platform</h1>
+    </>
+  );
+}
+
+export default App
---
+++ src/layouts/HomeLayouts.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+const HomeLayouts = () => {
+    return (
+        <div>
+            <nav>
+                <Navbar></Navbar>
+            </nav>
+            
+        </div>
+    );
+};
+
+export default HomeLayouts;
